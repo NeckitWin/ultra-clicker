ab131d3f38c35442c982fba1cd7bef430bd06d61
NeckitWin <neckit.v@gmail.com>
2024-02-02 19:48:05 +0100

    fix shop

--- js/clicker.js
+++ js/clicker.js
@@ -56,28 +56,32 @@ function clearScore() {
 }
 
 function buyDoubleClick() {
-    if (count >= 500) {
-        count -= 500;
-        updateMoney();
-        doubleClick = true;
-        saveDoubleClick();
-    } else if (doubleClick === true) {
-        alert("You have already purchased this")
+    if (!doubleClick) {
+        if (count >= 100) {
+            count -= 100;
+            updateMoney();
+            doubleClick = true;
+            saveDoubleClick();
+        } else {
+            alert("You don't have enough money");
+        }
     } else {
-        alert("You don't have enough money");
+        alert("You have already purchased this")
     }
 }
 
 function buyBusiness() {
-    if (count >= 1000) {
-        count -= 1000;
-        updateMoney();
-        business = true;
-        saveBusiness();
-    } else if (business === true) {
+    if (!business) {
+        if (count >= 1000) {
+            count -= 1000;
+            updateMoney();
+            business = true;
+            saveBusiness();
+        } else {
+            alert("You don't have enough money");
+        }
+    } else if (business) {
         alert("You have already purchased this")
-    } else {
-        alert("You don't have enough money");
     }
 }
 
@@ -94,30 +98,30 @@ document.addEventListener('click', function (event) {
     img.style.height = '20px';
     img.style.width = '20px';
     img.style.position = 'absolute';
-    var rand = Math.random()*(10-(-10)+1)+(-10);
-    img.style.left = x+rand+'px';
-    img.style.top = y+rand+'px';
+    var rand = Math.random() * (10 - (-10) + 1) + (-10);
+    img.style.left = x + rand + 'px';
+    img.style.top = y + rand + 'px';
     img.style.zIndex = '1';
     img.style.opacity = '1';
 
     document.body.appendChild(img);
 
-    anime(img,y);
+    anime(img, y);
 });
 
-function anime(img,y){
+function anime(img, y) {
     var opacity = 1;
     var top = y;
 
-    var animeInterval = setInterval(function (){
-        top+=1;
-        opacity-=0.05;
+    var animeInterval = setInterval(function () {
+        top += 1;
+        opacity -= 0.05;
 
-        img.style.top = top+'px';
+        img.style.top = top + 'px';
         img.style.opacity = opacity;
-        img.style.rotate = (Math.random()*(360-1+1)+1) + 'px';
+        img.style.rotate = (Math.random() * (360 - 1 + 1) + 1) + 'px';
 
-        if (opacity<=0){
+        if (opacity <= 0) {
             clearInterval(animeInterval);
             document.body.removeChild(img);
         }
